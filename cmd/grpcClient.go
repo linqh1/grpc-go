dfcc4c1ad32692bba6d24b6cf594e536656bea00
linqh1 <qihua.lin@quantil.com>
2019-12-30 16:39:31 +0800

    补充server端重启的案例测试

--- cmd/grpcClient.go
+++ cmd/grpcClient.go
@@ -12,10 +12,12 @@ import (
 var list = []string{"shindou", "中文", "kafka", "otter", "golang"}
 
 func main() {
-	conn, err := grpc.Dial("hub.qtlcdn.com:8888", grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:8888", grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
+	log.Printf("[grpc client] dial to server success.sleep 10 senconds and restart server")
+	time.Sleep(10 * time.Second)
 	grpcclient := grpcservice.NewGreeterClient(conn)
 	for _, v := range list {
 		go runClient(grpcclient, v)
@@ -31,7 +33,7 @@ func runClient(grpcclient grpcservice.GreeterClient, name string) {
 			time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
 			continue
 		}
-		log.Printf("[grpc client]received grpc server reply: %#v\n", reply.Message)
+		log.Printf("[grpc client] received grpc server reply: %#v\n", reply.Message)
 		time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
 	}
 }
