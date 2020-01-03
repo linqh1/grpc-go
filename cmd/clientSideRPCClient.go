9867fa186dbd7fd3d7b016801aa74ee0832fa813
linqh1 <qihua.lin@quantil.com>
2020-01-03 11:28:34 +0800

    多协程并发示例

--- cmd/clientSideRPCClient.go
+++ cmd/clientSideRPCClient.go
@@ -14,30 +14,37 @@ func main() {
 		panic(err)
 	}
 	client := clientside.NewFileBatchUploadClient(conn)
+	for i := 0; i < 10; i++ {
+		run1(i, client)
+	}
+	select {}
+}
+
+func run1(goroutineIndex int, client clientside.FileBatchUploadClient) {
 	uploadClient, err := client.Upload(context.Background())
 	if err != nil {
 		panic(err)
 	}
 	for _, v := range []string{"1.png", "2.css", "3.gif"} {
-		log.Printf("[grpc client-side stream client] send:%v\n", v)
+		log.Printf("[grpc client-side stream client-%v] send:%v\n", goroutineIndex, v)
 		err := uploadClient.Send(&clientside.FileInfo{
 			Name: v,
 		})
 		if err == io.EOF {
-			log.Printf("[grpc client-side stream client] receive end:%v\n", err)
+			log.Printf("[grpc client-side stream client-%v] receive end:%v\n", goroutineIndex, err)
 			break
 		}
 		if err != nil {
-			log.Printf("[grpc client-side stream client] send error:%v\n", err)
+			log.Printf("[grpc client-side stream client-%v] send error:%v\n", goroutineIndex, err)
 			break
 		}
 	}
 	reply, err := uploadClient.CloseAndRecv()
 	if err == io.EOF {
-		log.Printf("[grpc client-side stream client] CloseAndRecv success\n")
+		log.Printf("[grpc client-side stream client-%v] CloseAndRecv success\n", goroutineIndex)
 	} else if err != nil {
-		log.Fatalf("[grpc client-side stream client] %v.CloseAndRecv() got error %v, want %v", reply, err, nil)
+		log.Fatalf("[grpc client-side stream client-%v] %v.CloseAndRecv() got error %v, want %v", goroutineIndex, reply, err, nil)
 	} else {
-		log.Printf("[grpc client-side stream client] receive: %v\n", *reply)
+		log.Printf("[grpc client-side stream client-%v] receive: %v\n", goroutineIndex, *reply)
 	}
 }
